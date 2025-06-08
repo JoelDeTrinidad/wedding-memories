6ffeacb47078ab6a1de3f29e2cf841ce64a346d9
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2025-06-08 02:05:21 +0200

    key

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,19 +1,29 @@
-// firebase/config.ts
+// lib/firebaseConfig.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+    apiKey: "AIzaSyCw5PHqJgLuzWskFlUYsbte6AxhWki1QCg",
+    authDomain: "boda-momentos.firebaseapp.com",
+    projectId: "boda-momentos",
+    storageBucket: "boda-momentos.firebasestorage.app",
+    messagingSenderId: "217601370400",
+    appId: "1:217601370400:web:e3a565a7d12b53b8214f45",
 };
+// Initialize Firebase
+let firebaseApp;
+if (!getApps().length) {
+    firebaseApp = initializeApp(firebaseConfig);
+} else {
+    firebaseApp = getApp();
+}
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
-const storage = getStorage(app);
+const firestore = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
+const db = getFirestore(firebaseApp);
 
-export { db, storage };
+export { firebaseApp, firestore, auth, storage, db };
+
